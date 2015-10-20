692018368ec5ccae8533917ee32064e71bf08d24
dmejiaamador <dmejiaamador@gmail.com>
2015-10-19 18:14:42 -0700

    added player  and basket image

--- js/game/states/Game.js
+++ js/game/states/Game.js
@@ -5,5 +5,11 @@ paperToss.Game.prototype = {
         this.background = this.add.sprite(0,0,'background');
         this.background.scale.setTo(5);
         this.add.text(16, 16, "Current State: Game.", { font: "16px Arial", fill: "#ffffff" });
+        
+        this.player = this.add.sprite(this.game.width/2, 100 , 'player');
+        this.player = this.player.anchor.setTo(0.5);
+        
+        this.basket = this.add.sprite(this.game.width/2, this.game.height-100, 'basket');
+        this.basket = this.basket.anchor.setTo(.5);
     }
 }
--- js/game/states/Preload.js
+++ js/game/states/Preload.js
@@ -5,6 +5,8 @@ paperToss.Preload.prototype = {
         // here you load all the game images and what not
         //note to self you have to preload all the assets you want to use in the state before. Then you can actually use them. Cant use             them if you preload and create them in the same state.
         this.load.image('background', 'assets/images/background.png');
+        this.load.image('player', 'assets/images/player.png');
+        this.load.image('basket', 'assets/images/basket.png');
     
         this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY+ 128, 'preloadBar');
         this.preloadBar = this.preloadBar.anchor.setTo(0.5);
